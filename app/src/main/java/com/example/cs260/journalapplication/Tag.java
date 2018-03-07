f300c587637e0d2559c976a05286b0d95f41c57c
Nicolas Cerquera Angarita <cerqueraangarita@gmail.com>
2018-03-06 21:18:34 -0700

    Created new Tag class Test variable etc.

---
+++ app/src/main/java/com/example/cs260/journalapplication/Tag.java
@@ -0,0 +1,20 @@
+package com.example.cs260.journalapplication;
+
+import android.support.annotation.NonNull;
+
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+
+/**
+ * Created by cerqu on 6/03/2018.
+ */
+
+public class Tag {
+    String testVariable = "This is a test of commiting code";
+
+    public static void AddaTag(String tag){
+        String temp = tag;
+    }
+}
