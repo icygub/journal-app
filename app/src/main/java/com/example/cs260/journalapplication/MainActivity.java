092110c2604ef6ec4c974825704cb93738b9f664
icygub <jchubbs95@gmail.com>
2018-03-21 18:10:05 -0600

    can select and see video

--- app/src/main/java/com/example/cs260/journalapplication/MainActivity.java
+++ app/src/main/java/com/example/cs260/journalapplication/MainActivity.java
@@ -1,17 +1,115 @@
 package com.example.cs260.journalapplication;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+import android.widget.VideoView;
 
 import java.util.Scanner;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Intent intent;
+    private VideoView videoView;
+    private static final int SELECT_VIDEO = 1;
+    private static final int READ_REQUEST_CODE = 1;
+    private static final int REQUEST_TAKE_GALLERY_VIDEO = 2;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        videoView = findViewById(R.id.video_view);
     }
+
+    public void getVideoButtonClicked(View v) {
+        //Toast.makeText(this, "HELLO JORDAN", Toast.LENGTH_SHORT).show();
+
+//        intent = new Intent();
+//        intent.setType("video/*");
+//        intent.setAction(Intent.ACTION_GET_CONTENT);
+//        startActivityForResult(Intent.createChooser(intent,"Select Video"),REQUEST_TAKE_GALLERY_VIDEO);
+
+//        intent = new Intent(Intent.ACTION_PICK, MediaStore.Video.Media.EXTERNAL_CONTENT_URI);
+//        startActivityForResult(intent, SELECT_VIDEO);
+
+        Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+        intent.setType("video/*");
+        startActivityForResult(intent, READ_REQUEST_CODE);
+
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode,
+                                 Intent resultData) {
+
+        // The ACTION_OPEN_DOCUMENT intent was sent with the request code
+        // READ_REQUEST_CODE. If the request code seen here doesn't match, it's the
+        // response to some other intent, and the code below shouldn't run at all.
+
+        if (requestCode == READ_REQUEST_CODE && resultCode == Activity.RESULT_OK) {
+            // The document selected by the user won't be returned in the intent.
+            // Instead, a URI to that document will be contained in the return intent
+            // provided to this method as a parameter.
+            // Pull that URI using resultData.getData().
+            Uri uri = null;
+            if (resultData != null) {
+                uri = resultData.getData();
+                videoView.setVideoURI(uri);
+                videoView.requestFocus();
+                videoView.start();
+                //Log.i(TAG, "Uri: " + uri.toString());
+                //showImage(uri);
+            }
+        }
+    }
+
+
+//    @Override
+//    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+//        if (resultCode == RESULT_OK) {
+//            if (requestCode == REQUEST_TAKE_GALLERY_VIDEO) {
+//                Uri selectedImageUri = data.getData();
+//
+//                // OI FILE Manager
+//                filemanagerstring = selectedImageUri.getPath();
+//
+//                // MEDIA GALLERY
+//                selectedImagePath = getPath(selectedImageUri);
+//                if (selectedImagePath != null) {
+//
+//                    Intent intent = new Intent(HomeActivity.this,
+//                            VideoplayAvtivity.class);
+//                    intent.putExtra("path", selectedImagePath);
+//                    startActivity(intent);
+//                }
+//            }
+//        }
+//    }
+//
+//    // UPDATED!
+//    public String getPath(Uri uri) {
+//        String[] projection = { MediaStore.Video.Media.DATA };
+//        Cursor cursor = getContentResolver().query(uri, projection, null, null, null);
+//        if (cursor != null) {
+//            // HERE YOU WILL GET A NULLPOINTER IF CURSOR IS NULL
+//            // THIS CAN BE, IF YOU USED OI FILE MANAGER FOR PICKING THE MEDIA
+//            int column_index = cursor
+//                    .getColumnIndexOrThrow(MediaStore.Video.Media.DATA);
+//            cursor.moveToFirst();
+//            return cursor.getString(column_index);
+//        } else
+//            return null;
+//    }
 
     /**
      * This main method is for testing in the counsel all code needs to be placed within the MainActivity and attached to the UI
