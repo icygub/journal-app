48ab77e177732cb723070a0acfe4922704a3731f
Nicolas Cerquera Angarita <cerqueraangarita@gmail.com>
2018-03-26 17:43:58 -0600

    Started the main methods for pulling and adding a Audio to the Journal Entry

--- app/src/main/java/com/example/cs260/journalapplication/JournalEntry.java
+++ app/src/main/java/com/example/cs260/journalapplication/JournalEntry.java
@@ -1,6 +1,12 @@
 package com.example.cs260.journalapplication;
 
+import android.app.Activity;
+import android.content.Intent;
+
 import java.time.LocalDateTime;
+import java.util.List;
+
+import static android.support.v4.app.ActivityCompat.startActivityForResult;
 
 /**
  * Created by matthewruben on 3/5/18.
@@ -9,6 +15,7 @@ import java.time.LocalDateTime;
 public class JournalEntry {
     private LocalDateTime dateTime;
     private String text;
+    private List<String> audiosList;
 
     /**
      * Constructor
@@ -29,6 +36,15 @@ public class JournalEntry {
 
     public void setText(String text) {
         this.text = text;
+    }
+
+    public void addAudioFile(){
+        final int PICK_CONTACT_REQUEST = 1;
+
+        Intent intent;
+        intent = new Intent();
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        intent.setType("audio/*");
     }
 
     @Override
